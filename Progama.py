2549c9cc9e9052eb61ad5cfdfe2a6e8f7ab0f16c
OnOOF <19161295@itoaxaca.edu.mx>
2020-05-10 22:01:55 -0500

    subiendo

---
+++ Progama.py
@@ -0,0 +1,11 @@
+numero = int(input('Teclear un numero para realizar suma: '))
+numero2= int(input('Tecle el segudo numero de para la suma: '))
+suma=numero+numero2
+print("la suma es",suma)
+
+numerof= int(input('tecle el factorial: '))
+total=0
+for i in range(1,i<=numerof):
+    total=total+i
+
+print("resultado es",total)
---
+++ tarea.py
@@ -0,0 +1,9 @@
+
+numerof= int(input('tecle el factorial: '))
+total=0
+for i in range(0,numerof):
+     total=total+i
+
+
+total=total+numerof
+print("resultado es",total)
